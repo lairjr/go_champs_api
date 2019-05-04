79643dd9957cc67096d06077aad624372642767c
Lair Júnior <lairjr@outlook.com>
2019-05-04 02:55:06 -0400

    Allow update tournament group of tournament team

--- lib/tournamentsapi/tournaments.ex
+++ lib/tournamentsapi/tournaments.ex
@@ -265,6 +265,7 @@ defmodule TournamentsApi.Tournaments do
   def update_tournament_team(%TournamentTeam{} = tournament_team, attrs) do
     tournament_team
     |> TournamentTeam.changeset(attrs)
+    |> Ecto.Changeset.put_change(:tournament_group, attrs["tournament_group_id"])
     |> Repo.update()
   end
 
