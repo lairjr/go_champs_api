bd10e25c68a19ad69790bc242ee913469b9f4b8b
Lair Júnior <lairjr@outlook.com>
2019-07-21 13:04:07 -0400

    Fix tournament game view for rendering team without stats

--- lib/tournamentsapi_web/views/tournament_game_view.ex
+++ lib/tournamentsapi_web/views/tournament_game_view.ex
@@ -1,7 +1,6 @@
 defmodule TournamentsApiWeb.TournamentGameView do
   use TournamentsApiWeb, :view
   alias TournamentsApiWeb.TournamentGameView
-  alias TournamentsApiWeb.TournamentTeamView
 
   def render("index.json", %{tournament_games: tournament_games}) do
     %{data: render_many(tournament_games, TournamentGameView, "tournament_game.json")}
@@ -12,14 +11,19 @@ defmodule TournamentsApiWeb.TournamentGameView do
   end
 
   def render("tournament_game.json", %{tournament_game: tournament_game}) do
+    IO.inspect(tournament_game)
     %{
       id: tournament_game.id,
-      away_team:
-        render_one(tournament_game.away_team, TournamentTeamView, "tournament_team.json"),
+      away_team: %{
+        id: tournament_game.away_team.id,
+        name: tournament_game.away_team.name
+      },
       away_score: tournament_game.away_score,
       datetime: tournament_game.datetime,
-      home_team:
-        render_one(tournament_game.home_team, TournamentTeamView, "tournament_team.json"),
+      home_team: %{
+        id: tournament_game.home_team.id,
+        name: tournament_game.home_team.name
+      },
       home_score: tournament_game.home_score,
       location: tournament_game.location
     }
