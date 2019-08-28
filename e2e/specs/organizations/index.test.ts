0197c51e62a2298ea66d1ab02cd43bacd709b28a
Lair Júnior <lairjr@outlook.com>
2019-08-27 23:46:33 -0400

    Refacto api calls to its own file

--- e2e/specs/organizations/index.test.ts
+++ e2e/specs/organizations/index.test.ts
@@ -1,7 +1,5 @@
-import axios from 'axios';
 import { expect, tv4, use } from "chai";
-import { ORGANIZATIONS_URL } from "../URLs.js";
-import { randomString } from "../utils/random.js";
+import httpClient, { organizationPayload } from "./httpClient";
 import schema from "./organization_swagger.json";
 import ChaiJsonSchema = require("chai-json-schema");
 
@@ -9,39 +7,21 @@ use(ChaiJsonSchema);
 
 tv4.addSchema("#/definitions/Organization", schema.definitions.Organization);
 
-const createOrganizationPayload = () => ({
-  name: randomString(),
-  slug: randomString(),
-});
-
 describe("Organizations", () => {
-  const deleteOrganization = (organizationId: string) => {
-    return axios.delete(`${ORGANIZATIONS_URL}/${organizationId}`);
-  };
-
-  const createOrganization = () => {
-    const payload = {
-      organization: createOrganizationPayload(),
-    };
-    return axios.post(ORGANIZATIONS_URL, payload);
-  };
-
   describe("POST /", () => {
     it("matches schema", async () => {
-      const payload = {
-        organization: createOrganizationPayload(),
-      };
-      const { status, data } = await axios.post(ORGANIZATIONS_URL, payload);
+      const payload = organizationPayload();
+      const { status, data } = await httpClient.post(payload);
       expect(payload).to.be.jsonSchema(schema.definitions.OrganizationRequest);
       expect(status).to.be.equal(201);
       expect(data).to.be.jsonSchema(schema.definitions.OrganizationResponse);
-      await deleteOrganization(data.data.id);
+      await httpClient.delete(data.data.id);
     });
   });
 
   describe("GET /", () => {
     it("matches schema", async () => {
-      const { status, data } = await axios.get(ORGANIZATIONS_URL);
+      const { status, data } = await httpClient.getAll();
       expect(data).to.be.jsonSchema(schema.definitions.OrganizationsResponse);
       expect(status).to.be.equal(200);
     });
@@ -49,8 +29,8 @@ describe("Organizations", () => {
 
   describe("GET /:id", () => {
     it("matches schema", async () => {
-      const { data } = await createOrganization();
-      const { status, data: response } = await axios.get(`${ORGANIZATIONS_URL}/${data.data.id}`);
+      const { data: created } = await httpClient.post(organizationPayload());
+      const { status, data: response } = await httpClient.get(created.data.id);
       expect(response).to.be.jsonSchema(schema.definitions.OrganizationResponse);
       expect(status).to.be.equal(200);
     });
@@ -58,23 +38,20 @@ describe("Organizations", () => {
 
   describe("PATCH /:id", () => {
     it("matchs schema", async () => {
-      const { data } = await createOrganization();
-      const payload = {
-        organization: createOrganizationPayload(),
-      };
-      const { status, data: response } =
-        await axios.patch(`${ORGANIZATIONS_URL}/${data.data.id}`, payload);
+      const { data: created } = await httpClient.post(organizationPayload());
+      const payload = organizationPayload();
+      const { status, data: response } = await httpClient.patch(created.data.id, payload);
       expect(payload).to.be.jsonSchema(schema.definitions.OrganizationRequest);
       expect(response).to.be.jsonSchema(schema.definitions.OrganizationResponse);
       expect(status).to.be.equal(200);
-      await deleteOrganization(data.data.id);
+      await httpClient.delete(created.data.id);
     });
   });
 
   describe("DELETE /", () => {
     it("matches schema", async () => {
-      const { data } = await createOrganization();
-      const { status } = await axios.delete(`${ORGANIZATIONS_URL}/${data.data.id}`);
+      const { data: created } = await httpClient.post(organizationPayload());
+      const { status } = await httpClient.delete(created.data.id);
       expect(status).to.be.equal(204);
     });
   });
