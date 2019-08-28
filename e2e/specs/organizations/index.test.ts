32440f173ca523438bd2dee9af6de3e66461f612
Lair Júnior <lairjr@outlook.com>
2019-08-27 22:22:23 -0400

    Change to match json schema to API response

--- e2e/specs/organizations/index.test.ts
+++ e2e/specs/organizations/index.test.ts
@@ -1,12 +1,20 @@
-import { expect, use } from "chai";
+import axios from 'axios';
+import { expect, tv4, use } from "chai";
 import schema from "./organization_swagger.json";
 import ChaiJsonSchema = require("chai-json-schema");
 
 use(ChaiJsonSchema);
 
+const ORGANIZATIONS_URL = "https://yochamps-api.herokuapp.com/api/organizations";
+
+tv4.addSchema("#/definitions/Organization", schema.definitions.Organization);
+
 describe("Organizations", () => {
-  it("matchs hardcoded respose", () => {
-    const organization = schema.definitions.OrganizationRequest.example.organization;
-    expect(organization).to.be.jsonSchema(schema.definitions.Organization);
+  describe("/get", () => {
+    it("matches schema", async () => {
+      const { status, data } = await axios.get(ORGANIZATIONS_URL);
+      expect(data).to.be.jsonSchema(schema.definitions.OrganizationsResponse);
+      expect(status).to.be.equal(200);
+    });
   });
 });
