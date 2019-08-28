ba5fbb9d6d00fd608ffd5fd27af8e1c20a39a2f7
Lair Júnior <lairjr@outlook.com>
2019-08-27 22:50:47 -0400

    Add test for post organization

--- e2e/specs/organizations/index.test.ts
+++ e2e/specs/organizations/index.test.ts
@@ -9,12 +9,33 @@ const ORGANIZATIONS_URL = "https://yochamps-api.herokuapp.com/api/organizations"
 
 tv4.addSchema("#/definitions/Organization", schema.definitions.Organization);
 
+const randomString = () => (
+  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+);
+
+const createOrganizationPayload = () => ({
+  name: randomString(),
+  slug: randomString(),
+});
+
 describe("Organizations", () => {
-  describe("/get", () => {
+  describe("GET /", () => {
     it("matches schema", async () => {
       const { status, data } = await axios.get(ORGANIZATIONS_URL);
       expect(data).to.be.jsonSchema(schema.definitions.OrganizationsResponse);
       expect(status).to.be.equal(200);
+    });
+  });
+
+  describe("POST", () => {
+    it("matches schema", async () => {
+      const payload = {
+        organization: createOrganizationPayload(),
+      };
+      const { status, data } = await axios.post(ORGANIZATIONS_URL, payload);
+      expect(payload).to.be.jsonSchema(schema.definitions.OrganizationRequest);
+      expect(status).to.be.equal(201);
+      expect(data).to.be.jsonSchema(schema.definitions.OrganizationResponse);
     });
   });
 });
