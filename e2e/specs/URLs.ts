f6b9a21b2977af07683c6f3b4a55ecf3b81bbb02
Lair Júnior <lairjr@outlook.com>
2020-04-12 16:50:21 -0400

    Change base api host on contract test

--- e2e/specs/URLs.ts
+++ e2e/specs/URLs.ts
@@ -1,11 +1,10 @@
-const API_HOST = process.env.API_HOST || "http://localhost:4000/api";
-const API_V1_HOST = "http://phoenix:4000/v1";
+const API_HOST = process.env.API_HOST || "http://localhost:4000/v1";
 
-export const DRAWS_URL = `${API_V1_HOST}/draws`;
-export const ELIMINATIONS_URL = `${API_V1_HOST}/eliminations`;
-export const GAMES_URL = `${API_V1_HOST}/games`;
-export const ORGANIZATIONS_URL = `${API_V1_HOST}/organizations`;
-export const PHASES_URL = `${API_V1_HOST}/phases`;
-export const TEAMS_URL = `${API_V1_HOST}/teams`;
-export const TOURNAMENTS_URL = `${API_V1_HOST}/tournaments`;
-export const USERS_URL = `${API_V1_HOST}/users`;
+export const DRAWS_URL = `${API_HOST}/draws`;
+export const ELIMINATIONS_URL = `${API_HOST}/eliminations`;
+export const GAMES_URL = `${API_HOST}/games`;
+export const ORGANIZATIONS_URL = `${API_HOST}/organizations`;
+export const PHASES_URL = `${API_HOST}/phases`;
+export const TEAMS_URL = `${API_HOST}/teams`;
+export const TOURNAMENTS_URL = `${API_HOST}/tournaments`;
+export const USERS_URL = `${API_HOST}/users`;
